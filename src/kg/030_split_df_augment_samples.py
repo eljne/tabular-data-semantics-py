7fde698ea75539172de1ba90cdd462fbef19ce67
El Bill <eleanorjbill@gmail.com>
2020-09-08 14:01:04 +0100

    Create negative samples

--- src/kg/030_split_df_augment_samples.py
+++ src/kg/030_split_df_augment_samples.py
@@ -5,77 +5,103 @@ import pickle
 import random
 import numpy as np
 from ontology.onto_access import DBpediaOntology
+from owlready2 import *
 
 # read csv file
 df = pd.read_csv('data/df.csv')
 
 print('done read from csv')
-
-# check for missing values etc.
-df.isnull().sum()
-
-dict_of_types = dict(iter(df.groupby('type')))
-dict_of_categories = dict(iter(df.groupby('category')))
-
-print('done split to types and categories')
-
-'''test different strategies to augment positive and negative samples'''
+#
+# # check for missing values etc.
+# df.isnull().sum()
+#
+# dict_of_types = dict(iter(df.groupby('type')))
+# dict_of_categories = dict(iter(df.groupby('category')))
+#
+# print('done split to types and categories')
+#
+# '''test different strategies to augment positive and negative samples'''
 
 # query for siblings of target types
 # siblings of the classes associated with the target types e.g. ‘basketball player’ vs. ‘football player’
-uri_onto = "/home/GitHub/tabular-data-semantics-py/TabularSemantics/ontologies/dbpedia_2014_fix.owl"
-onto_access = DBpediaOntology()
-onto_access.loadOntology(True)
-
-
-def get_sibling(type):
-    if type != "''":
-        ancestors = onto_access.getDescendantNamesForClassName(type)
-        # ancestors = onto_access.getClassByName(type).ancestors())
-        # get ancestor
-        ancestor = ancestors.pop()
-        # siblings = onto_access.getDescendantNames(ancestor)
-        siblings = onto_access.getClassByName(ancestor).descendants()
-        sibling = siblings.pop()
-        if sibling == type:
-            sibling = siblings.pop()
-    else:
-        return ''
-    return sibling
-
-
-df['sibling_type'] = get_sibling(df['type'])
-
-
-# also members of the same type e.g. different basketball players
-# def get_member_sametype(type):
+# uri_onto="http://www.cs.ox.ac.uk/isg/ontologies/schema.org.owl"
+# onto_access = DBpediaOntology()
+# onto_access.loadOntology(True)
 #
-#     return member
+#
+# def get_sibling(type):
+#     type2 = type.split('\'')
+#     types = []
+#     labels = []
+#     for t in type2:
+#         type3 = t.replace("[", "")
+#         type4 = type3.replace(",", "")
+#         type5 = type4.replace("]", "")
+#         type6 = type5.replace(" ", "")
+#         type7 = type6.replace("\n", "")
+#         types.append(type7)
+#     for t in types:
+#         while "" in types:
+#             types.remove("")
+#         t2 = str(t.replace("dbo:", ""))
+#         # print('t2', t2)
+#         try:
+#             # print("\n 1")
+#             ancestors = onto_access.getAncestorsURIs(onto_access.getClassByName(t2))
+#             # print('ancestors', ancestors)
+#             ancestor = random.sample(ancestors, k=1)
+#             # print('ancestor', ancestor[0])
+#             cl8ss = onto_access.getClassByURI(ancestor[0])
+#             siblings = onto_access.getDescendantNames(cl8ss)
+#             # print('siblings', siblings)
+#             sibling = random.sample(siblings, k=1)
+#             # print('sibling', sibling[0])
+#             while sibling[0] == t:
+#                  sibling = random.sample(siblings, k=1)
+#             labels.append(sibling[0])
+#         except:
+#             try:
+#                 # print("\n 2")
+#                 similar = onto_access.getClassIRIsContainingName(t2)
+#                 labels = onto_access.getDescendantNames(similar)
+#                 label = random.sample(labels, k=1)
+#                 labels.append(label[0])
+#                 # print(label[0])
+#             except:
+#                 pass
+#     # print('\n types', types)
+#     # print('labels', labels)
+#     return labels
+#
+#
+# df['sibling_type'] = df['type'].apply(get_sibling)   # column by column
+
 
 # shuffle the answer categories and types independent of the questions to get a random answer category/type
-
-
-def shuffle(df, df_column, output_column):
+def shuffle(df_column):
+    df['output_column'] = df_column
     n_rows = len(df_column)
-    pick_new_rows = random.sample(range(1, n_rows), n_rows)
-    df.loc[pick_new_rows, output_column] = np.random.permutation(df.loc[pick_new_rows, output_column])
+    pick_new_rows = np.random.permutation(list(range(n_rows)))[0:n_rows]
+    # df.loc[pick_new_rows, 'output_column'] = np.random.permutation(df.loc[pick_new_rows, 'output_column'])
+    shuffled_values = np.random.permutation(df['output_column'][pick_new_rows])
+    df['output_column'][pick_new_rows] = shuffled_values
     return df
 
 
-df2 = shuffle(df, df['type'], 'shuffled_type')
+df2 = df['type'].apply(shuffle)
+print('done shuffle')
 
-# ep.getTopTypesUsingPredicatesForObject(ent, 3)
+df2.to_csv('data/df2.csv')
+print('done read to csv')
 
-
-
-
-
-# pickle
-
-f = open('data/positive_samples.pkl', 'rb')
-pickle.dump(positive_samples,f)
-f.close()
-
-f = open('data/negative_samples.pkl', 'rb')
-pickle.dump(negative_samples,f)
-f.close()
+# separate positive and negative samples
+#
+# # pickle
+#
+# f = open('data/positive_samples.pkl', 'rb')
+# pickle.dump(positive_samples,f)
+# f.close()
+#
+# f = open('data/negative_samples.pkl', 'rb')
+# pickle.dump(negative_samples,f)
+# f.close()
--- src/ontology/onto_access.py
+++ src/ontology/onto_access.py
@@ -204,18 +204,84 @@ if __name__ == '__main__':
     #onto_access = SchemaOrgOntology()
     
     onto_access.loadOntology(True)
-    
-    
+
+    onto_access.getAncestorsURIs(onto_access.getClassByName("gymnast")) #ancestors
+
     
     print(onto_access.getClassIRIsContainingName("player"))
     print(onto_access.getClassIRIsContainingName("actor"))
+
+
     
-    
-    
-    print('1',onto_access.getClassByName("City"))
-    print('2',onto_access.getClassByName("City").descendants())
-    print('3',onto_access.getClassByName("City").ancestors())
-    print('4',onto_access.getDescendantURIs(onto_access.getClassByName("City")))
-    print('5',onto_access.getAncestorsURIs(onto_access.getClassByName("City")))
-    
-    
+    #
+    # print('1', onto_access.getClassByName("gymnast"))
+    # print('2', onto_access.getClassByName("gymnast").descendants())
+    # print('3', onto_access.getClassByName("gymnast").ancestors())
+    # print('4', onto_access.getDescendantURIs(onto_access.getClassByName("gymnast")))
+
+    t = "gymnast"
+    print('ancestors', onto_access.getAncestorsURIs(onto_access.getClassByName(t)))
+    test = onto_access.getAncestorsURIs(onto_access.getClassByName(t))
+    test2 = test.pop()
+    print('ancestor', test2)
+    test3 = onto_access.getClassByURI(test2)
+    print(test3)
+
+    try:
+        siblings2 = onto_access.getDescendantNames(test3)
+        print('siblings2', siblings2)
+        sibling2 = siblings2.pop()
+        print('sibling2', sibling2)
+    except:
+        print(";)")
+
+'''
+    ancestors
+    {'http://schema.org/Person', 'http://www.wikidata.org/entity/Q215627', 'http://www.w3.org/2002/07/owl#Thing',
+     'http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#Agent', 'http://dbpedia.org/ontology/Athlete',
+     'http://www.wikidata.org/entity/Q24229398', 'http://dbpedia.org/ontology/Person',
+     'http://www.wikidata.org/entity/Q5', 'http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#NaturalPerson',
+     'http://xmlns.com/foaf/0.1/Person', 'http://dbpedia.org/ontology/Gymnast'}
+    ancestor
+    http: // schema.org / Person
+    schema.org.Person
+    siblings2
+    {'Q2526255', 'SongWriter', 'Lieutenant', 'FormulaOneRacer', 'NaturalPerson', 'Q3055126', 'HorseTrainer',
+     'RallyDriver', 'Q488111', 'Q214917', 'Q901', 'PokerPlayer', 'Q1930187', 'ComicsCharacter', 'Q2566598',
+     'Politician', 'HighDiver', 'Dancer', 'HandballPlayer', 'GridironFootballPlayer', 'PlayWright',
+     'MemberResistanceMovement', 'AthleticsPlayer', 'Q42178', 'Canoeist', 'BasketballPlayer', 'Ski_jumper', 'Q1028181',
+     'ChessPlayer', 'AustralianRulesFootballPlayer', 'Q3501317', 'Governor', 'Farmer', 'Deputy', 'TelevisionDirector',
+     'Producer', 'Q131512', 'VolleyballPlayer', 'Q15117302', 'Q14467526', 'Q28389', 'Cleric', 'SnookerPlayer', 'Q40348',
+     'Boxer', 'Curler', 'Sculptor', 'AmateurBoxer', 'Priest', 'SumoWrestler', 'Q14128148', 'President', 'NascarDriver',
+     'ChristianPatriarch', 'Biologist', 'Q15410431', 'Chef', 'Q4270517', 'Humorist', 'RugbyPlayer', 'Murderer',
+     'Q486839', 'Historian', 'ComicsCreator', 'Q753110', 'Poet', 'Engineer', 'Q4964182', 'Q16266334', 'Host', 'Q937857',
+     'Q33231', 'Q10871364', 'Q49757', 'TeamMember', 'Q3621491', 'Scientist', 'SpeedwayRider', 'Q378622', 'BeautyQueen',
+     'Artist', 'Comedian', 'Q132050', 'Person', 'Swimmer', 'FashionDesigner', 'AmericanFootballCoach', 'Painter',
+     'Skater', 'Q628099', 'Q13219587', 'MilitaryPerson', 'Q177220', 'Criminal', 'Coach', 'Ambassador', 'Singer',
+     'MartialArtist', 'Writer', 'Linguist', 'RadioHost', 'Aristocrat', 'MotorcycleRider', 'PrimeMinister',
+     'Archeologist', 'Q188094', 'Q16533', 'BeachVolleyballPlayer', 'Cricketer', 'DartsPlayer', 'Surfer', 'Astronaut',
+     'SoccerPlayer', 'Q4610556', 'TelevisionHost', 'NordicCombined', 'VoiceActor', 'RomanEmperor', 'TableTennisPlayer',
+     'Q842606', 'PoliticianSpouse', 'Entomologist', 'Skier', 'IceHockeyPlayer', 'Monarch', 'GaelicGamesPlayer',
+     'NationalCollegiateAthleticAssociationAthlete', 'Q121998', 'Q13156709', 'Q95074', 'Egyptologist', 'Senator',
+     'Q42603', 'VolleyballCoach', 'Q215627', 'Psychologist', 'Chancellor', 'Royalty', 'Gymnast', 'Q33999',
+     'AnimangaCharacter', 'PlayboyPlaymate', 'BusinessPerson', 'Q2159907', 'AdultActor', 'HorseRider', 'Q10833314',
+     'Actor', 'Q3282637', 'GolfPlayer', 'DTMRacer', 'Q483501', 'Referee', 'Q30461', 'Q1350189', 'BadmintonPlayer', 'Q5',
+     'NarutoCharacter', 'ArcherPlayer', 'Jockey', 'Noble', 'VicePresident', 'Q82955', 'CrossCountrySkier', 'Q10842936',
+     'Lawyer', 'BaseballPlayer', 'Q212980', 'ChristianBishop', 'Religious', 'Cyclist', 'Q13365117', 'Q211236',
+     'Q13415036', 'Q13382519', 'Baronet', 'Saint', 'SportsTeamMember', 'TheatreDirector', 'BullFighter', 'Medician',
+     'Q13414980', 'Biathlete', 'NetballPlayer', 'LacrossePlayer', 'Q43115', 'Q19546', 'SoccerManager', 'Guitarist',
+     'CanadianFootballPlayer', 'Athlete', 'MemberOfParliament', 'Model', 'OfficeHolder', 'Congressman', 'SpeedSkater',
+     'RacingDriver', 'Journalist', 'Fencer', 'DisneyCharacter', 'SerialKiller', 'Q1198887', 'Celebrity',
+     'SoapCharacter', 'MotocycleRacer', 'Q3665646', 'Q36180', 'Mayor', 'Q116', 'Q11631', 'ClassicalMusicArtist',
+     'Judge', 'Q855091', 'CollegeCoach', 'Q11774891', 'Presenter', 'Vicar', 'Q3387717', 'FictionalCharacter',
+     'ScreenWriter', 'Q484188', 'Pope', 'BritishRoyalty', 'MusicComposer', 'OrganisationMember', 'MotorsportRacer',
+     'Q737498Q13381863', 'SnookerChamp', 'MovieDirector', 'Bodybuilder', 'AmericanFootballPlayer', 'Rower',
+     'Photographer', 'WaterPoloPlayer', 'SportsManager', 'Q947873', 'Q847400', 'MusicDirector', 'SquashPlayer',
+     'Q2722764', 'Q13590141', 'TennisPlayer', 'Instrumentalist', 'Q373085', 'Q10843402', 'MythologicalFigure',
+     'VicePrimeMinister', 'Orphan', 'Q15982795', 'TelevisionPersonality', 'Q13382576', 'BackScene', 'Economist',
+     'Wrestler', 'Q13561328', 'BobsleighAthlete', 'Cardinal', 'Q81096', 'WinterSportPlayer', 'Architect',
+     'MusicalArtist', 'FigureSkater', 'Q30185', 'Professor', 'Q13474373', 'Philosopher', 'Q16278103'}
+    sibling2
+    Q2526255
+
+'''
