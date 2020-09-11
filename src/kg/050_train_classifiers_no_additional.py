4e1acc09cce3af9e3760ff14fe037b1e3e8e0097
El Bill <eleanorjbill@gmail.com>
2020-09-11 17:37:48 +0100

    Update 050_train_classifiers_no_additional.py

--- src/kg/050_train_classifiers_no_additional.py
+++ src/kg/050_train_classifiers_no_additional.py
@@ -19,7 +19,8 @@ def unique_array_workaround(series):
     series2 = []
     for item in series:
         item2 = str(item)
-        series2.append(item2)
+        if item2 != '[]' and item2 != []:   # get rid of label = []
+            series2.append(item2)
     typez = list(set(series2))
     return typez
 
@@ -59,22 +60,24 @@ def random_sample_ratioed(datafrm, pos_fraction, ratio_pos, ratio_neg):
 def train_classifier(train, label):
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
                         hidden_layer_sizes=(5, 2), random_state=1)
-    print('label', label.shape)   # array y of size (n_samples,)
+    # print('label', label.shape)   # array y of size (n_samples,)
     # 8814    0
     # 11152   0
     # 3949    [[0.0223, 0.0816, 0.0139, -0.0124, -0.0084, -0...
-    print('train', train.shape)   # array X of size (n_samples, n_features)
-    clf.fit(train, label)   # debug from here
+    # print('train', train.shape)   # array X of size (n_samples, n_features)
+    clf.fit(list(train), label)   # debug from here
     return clf
 
 
 '''select positive samples at random from training data'''
-classifiers_pos_cat = {}
-classifiers_pos_typ = {}
+classifiers_pos_typ = dict.fromkeys(types)
+classifiers_pos_cat = dict.fromkeys(categories)
+print('classifiers_pos_cat', classifiers_pos_cat)
+print('classifiers_pos_typ', classifiers_pos_typ)
 
 
-def label_polarity(row, label):
-    if row['category'] == label:  # if category is current label
+def label_polarity(row, label, column):
+    if row[column] == label or str(row[column]) == label:  # if category is current label
         return 1  # positive polarity
     else:
         return 0  # negative polarity
@@ -83,28 +86,41 @@ def label_polarity(row, label):
 for df in cats_dfs:
     copy_ds = positive_samples.copy()
     cat_label = df["category"].unique()
-    copy_ds["y"] = copy_ds.apply(lambda row: label_polarity(row, cat_label), axis=1)
+    copy_ds["y"] = copy_ds.apply(lambda row: label_polarity(row, cat_label, 'category'), axis=1)
     train_set = random_sample_ratioed(copy_ds, 0.80, 1, 1)  # split differently according to pos/neg balance
     # print(train_set)
     # X = train_set['concatenated_vector']
-    X = train_set['',,,,]
-    print(len(X))
+    X = train_set['we_np_vector']
     y = train_set["y"]
-    print(len(y))
     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
-    classifiers_pos_cat[cats_dfs] = classifier
+    classifiers_pos_cat[cat_label[0]] = classifier
+
+
+for df in types_dfs:
+    copy_ds2 = positive_samples.copy()
+    typ_label = unique_array_workaround(df["type"])
+    print('label', typ_label)
+    copy_ds2["y"] = copy_ds2.apply(lambda row: label_polarity(row, typ_label, 'type'), axis=1)  # get rows that match
+    # the type
+    train_set = random_sample_ratioed(copy_ds2, 0.80, 1, 1)  # split differently according to pos/neg balance
+    # print(train_set)
+    # X = train_set['concatenated_vector']
+    X = train_set['we_np_vector']
+    y = train_set["y"]
+    classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
+    classifiers_pos_typ[typ_label] = classifier
 
 # for df in types_dfs:
-# train_set, test_set = train_test(df, 0.80)
-# type_label = unique_array_workaround(df["type"])
-# print(type_label)
+#     type_label = unique_array_workaround(df["type"])
+#     print(type_label)
+
 # classifier = train_classifier(train_set['concatenated_vector'], type_label)
 # classifiers_pos_typ[type_label] = classifier
 
 
-f = open('data/classifiers_pos_cat.pkl', 'rb')
-pickle.dump(classifiers_pos_cat, f)
-pkl_file.close()
+# f = open('data/classifiers_pos_cat.pkl', 'rb')
+# pickle.dump(classifiers_pos_cat, f)
+# pkl_file.close()
 
 
 #
