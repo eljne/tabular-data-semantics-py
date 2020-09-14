a6249a80eccfa445a54cdb62b8e583eb44205000
El Bill <eleanorjbill@gmail.com>
2020-09-14 14:06:04 +0100

    Train classifiers on all types

--- src/kg/040_create_positive_samples.py
+++ src/kg/040_create_positive_samples.py
@@ -1,9 +1,11 @@
 ''' augment positive samples to create more positive sample '''
-# 9th september - ejb
+# 14th september - ejb
 # may not include this after all - ask Ernesto
 
 import pandas as pd
 import pickle
+import random
+from ontology.onto_access import DBpediaOntology
 
 # unpickle
 pkl_file = open('data/df.pkl', 'rb')
@@ -11,17 +13,54 @@ load = pickle.load(pkl_file)
 pkl_file.close()
 
 df_positive = pd.DataFrame(load)
-
 df_positive['polarity'] = "1"
 
-'''test different strategies to augment positive samples'''
+'''create more positive samples
+do this by:
+- getting a different entity (sibling?) using ontology
+'''
 
-# query for siblings of target types
-# get questions with different types in them e.g. who coached [athlete]? [coach]
-# siblings of the classes associated with the target types e.g. ‘basketball player’ vs. ‘football player’
-# uri_onto = "http://www.cs.ox.ac.uk/isg/ontologies/schema.org.owl"
-# onto_access = DBpediaOntology()
-# onto_access.loadOntology(True)
+uri_onto = "http://www.cs.ox.ac.uk/isg/ontologies/schema.org.owl"
+onto_access = DBpediaOntology()
+onto_access.loadOntology(True)
+
+
+def get_sibling(nps):
+    # ['political ideology', 'nazis']
+    # ['google knowledge graph id', 'martn vizcarra']
+    labels = []
+    for n in nps:
+        try:
+            # print("\n 1")
+            ancestors = onto_access.getAncestorsURIs(onto_access.getClassByName(n))
+            print('ancestors', ancestors)
+            ancestor = random.sample(ancestors, k=1)
+            # print('ancestor', ancestor[0])
+            cl8ss = onto_access.getClassByURI(ancestor[0])
+            siblings = onto_access.getDescendantNames(cl8ss)
+            # print('siblings', siblings)
+            sibling = random.sample(siblings, k=1)
+            # print('sibling', sibling[0])
+            while sibling[0] == t:
+                 sibling = random.sample(siblings, k=1)
+            labels.append(sibling[0])
+        except:
+            try:
+                # print("\n 2")
+                similar = onto_access.getClassIRIsContainingName(t2)
+                labels = onto_access.getDescendantNames(similar)
+                label = random.sample(labels, k=1)
+                labels.append(label[0])
+                # print(label[0])
+            except:
+                pass
+    # print('\n types', types)
+    # print('labels', labels)
+    return labels
+
+
+df_positive['sibling_entity'] = df_positive['np list'].apply(get_sibling)   # column by column
+print('done siblings got for entities')
 
 # pickle
 f = open('data/positive_samples.pkl', 'wb')
--- src/kg/050_train_classifiers_no_additional.py
+++ src/kg/050_train_classifiers_no_additional.py
@@ -15,35 +15,54 @@ positive_samples = pd.DataFrame(load)
 '''split on types/categories again'''
 
 
-def unique_array_workaround(series):
-    series2 = []
-    for item in series:
-        item2 = str(item)
-        if item2 != '[]' and item2 != []:   # get rid of label = []
-            series2.append(item2)
-    typez = list(set(series2))
-    return typez
+# def unique_array_workaround(series):
+#     series2 = []
+#     for item in series:
+#         item2 = str(item)
+#         if item2 != '[]' and item2 != []:   # get rid of label = []
+#             series2.append(item2)
+#     ty = list(set(series2))
+#     return ty
+
+
+def get_last(list):
+    try:
+        return str(list[-1])
+    except:
+        return None
 
 
 categories = positive_samples['category'].unique()
-types = unique_array_workaround(positive_samples['type'])
+positive_samples['fine type'] = positive_samples['type'].apply(get_last)
+types = positive_samples['fine type'].unique()
 
-positive_samples['type tuple'] = positive_samples['type'].map(tuple)
-types_dfs = [pd.DataFrame(y) for x, y in positive_samples.groupby('type tuple', as_index=False)]
+types_dfs = [pd.DataFrame(y) for x, y in positive_samples.groupby('fine type', as_index=False)] # just last one
 cats_dfs = [pd.DataFrame(y) for x, y in positive_samples.groupby('category', as_index=False)]
 
 print('done split to types and categories')
+
+
+def get_all(list):
+    for t in list:
+        types_all.append(t)
+    return 0
+
+# all types
+types_all = []
+positive_samples['type'].apply(get_all)
+types_all_unique = list(set(types_all))
+print('all types', types_all_unique)
 
 
 def random_sample_ratioed(datafrm, pos_fraction, ratio_pos, ratio_neg):
     # fraction is a ratio e.g.
     positive = datafrm[datafrm['y'] == 1]
     negative = datafrm[datafrm['y'] == 0]
+    print('len pos', len(positive))
+    print('len neg', len(negative))
     positive_smples = positive.sample(frac=pos_fraction, random_state=0)
     neg_samples_wanted = ((len(positive_smples) / ratio_pos) * ratio_neg)  # make sure this is correct! do some math!
     fraction2 = neg_samples_wanted / len(negative)
-    # print('pos', len(positive))
-    # print('neg', len(negative))
     # print('pos sampled', len(positive_smples))
     # print('neg wanted', neg_samples_wanted)
     try:
@@ -61,70 +80,83 @@ def train_classifier(train, label):
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
                         hidden_layer_sizes=(5, 2), random_state=1)
     # print('label', label.shape)   # array y of size (n_samples,)
-    # 8814    0
-    # 11152   0
-    # 3949    [[0.0223, 0.0816, 0.0139, -0.0124, -0.0084, -0...
     # print('train', train.shape)   # array X of size (n_samples, n_features)
     clf.fit(list(train), label)   # debug from here
     return clf
 
 
 '''select positive samples at random from training data'''
-classifiers_pos_typ = dict.fromkeys(types)
-classifiers_pos_cat = dict.fromkeys(categories)
-print('classifiers_pos_cat', classifiers_pos_cat)
-print('classifiers_pos_typ', classifiers_pos_typ)
-
 
 def label_polarity(row, label, column):
-    if row[column] == label or str(row[column]) == label:  # if category is current label
+    if row[column] == label or str(row[column]) == label:  # if type/category is current label
         return 1  # positive polarity
     else:
         return 0  # negative polarity
 
 
+def label_polarity_all_typs(row, label, column):
+    if label in row[column] or label in str(row[column]):  # if type is in current label
+        return 1  # positive polarity
+    else:
+        return 0  # negative polarity
+
+
+# dictionaries in which to store classifiers, arranges by type/category
+classifiers_pos_cat = dict.fromkeys(categories)
+print('classifiers_pos_cat', classifiers_pos_cat)
+
 for df in cats_dfs:
     copy_ds = positive_samples.copy()
     cat_label = df["category"].unique()
-    copy_ds["y"] = copy_ds.apply(lambda row: label_polarity(row, cat_label, 'category'), axis=1)
+    # print('cat label', cat_label)
+    copy_ds["y"] = copy_ds.apply(lambda row: label_polarity(row, cat_label, 'category'), axis=1)    # label +ve and -ve
     train_set = random_sample_ratioed(copy_ds, 0.80, 1, 1)  # split differently according to pos/neg balance
-    # print(train_set)
     # X = train_set['concatenated_vector']
     X = train_set['we_np_vector']
     y = train_set["y"]
     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
     classifiers_pos_cat[cat_label[0]] = classifier
 
+# just last (theoretically most fine-grained type)
+# for df in types_dfs:
+#     copy_ds2 = positive_samples.copy()
+#     typ_label = df["fine type"].unique()   # get the type associated with this df iteration
+#     copy_ds2["y"] = copy_ds2.apply(lambda row: label_polarity(row, typ_label, 'fine type'), axis=1)  # label -/+
+#     train_set = random_sample_ratioed(copy_ds2, 0.80, 1, 1)  # split differently according to pos/neg balance
+#     # X = train_set['concatenated_vector']
+#     X = train_set['we_np_vector']
+#     y = train_set["y"]
+#     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
+    # classifiers_pos_typ[typ_label] = classifier
 
-for df in types_dfs:
+
+types_all = set(types_all) # get unique values
+print(len(types_all)) # 310 unique types
+
+# dictionaries in which to store classifiers, arranges by type/category
+classifiers_pos_typ = dict.fromkeys(types)
+print('classifiers_pos_typ', classifiers_pos_typ)
+
+
+# all types: not just last type
+for typ_label in types_all:
     copy_ds2 = positive_samples.copy()
-    typ_label = unique_array_workaround(df["type"])
-    print('label', typ_label)
-    copy_ds2["y"] = copy_ds2.apply(lambda row: label_polarity(row, typ_label, 'type'), axis=1)  # get rows that match
-    # the type
+    print('type label', typ_label)
+    copy_ds2["y"] = copy_ds2.apply(lambda row: label_polarity_all_typs(row, typ_label, 'type'), axis=1)  # label -/+
     train_set = random_sample_ratioed(copy_ds2, 0.80, 1, 1)  # split differently according to pos/neg balance
-    # print(train_set)
     # X = train_set['concatenated_vector']
     X = train_set['we_np_vector']
     y = train_set["y"]
     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
     classifiers_pos_typ[typ_label] = classifier
 
-# for df in types_dfs:
-#     type_label = unique_array_workaround(df["type"])
-#     print(type_label)
 
-# classifier = train_classifier(train_set['concatenated_vector'], type_label)
-# classifiers_pos_typ[type_label] = classifier
+f = open('data/classifiers_pos_cat.pkl', 'rb')
+pickle.dump(classifiers_pos_cat, f)
+pkl_file.close()
 
 
-# f = open('data/classifiers_pos_cat.pkl', 'rb')
-# pickle.dump(classifiers_pos_cat, f)
-# pkl_file.close()
+f = open('data/classifiers_pos_typ.pkl', 'rb')
+pickle.dump(classifiers_pos_typ,f)
+pkl_file.close()
 
-
-#
-# f = open('data/classifiers_pos_typ.pkl', 'rb')
-# pickle.dump(classifiers_pos_typ,f)
-# pkl_file.close()
-#
