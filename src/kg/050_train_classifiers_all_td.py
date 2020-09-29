878f026bebb8696292b861e978a140f3973b51ea
El Bill <eleanorjbill@gmail.com>
2020-09-29 15:09:38 +0100

    Try and find bug

--- src/kg/050_train_classifiers_all_td.py
+++ src/kg/050_train_classifiers_all_td.py
@@ -1,7 +1,6 @@
 ''' author: Eleanor Bill @eljne '''
 ''' train a MLP model for each category and type '''
 import pandas as pd
-import numpy as np
 from kg.EB_classes import unpickle, get_last, pickl
 from sklearn.neural_network import MLPClassifier
 
@@ -13,24 +12,21 @@ vector_component = 'we_nouns_vector'
 # we_type_vector
 # concatenated_vector
 
-# use all training data
-# all_td = unpickle('training_vectors/32_all_td_justconcat')
-all_td = unpickle('training_vectors/31_all_td_fin')
+# all_td = unpickle('training_vectors/32_all_td_justconcat') # for using the concatenated vector
+all_td = unpickle('training_vectors/31_all_td_fin') # use all training data
 all_samples = pd.DataFrame(all_td)
 
-
 '''split on types/categories again'''
-
 categories = all_samples['category'].unique()
 all_samples['fine type'] = all_samples['type'].apply(get_last)
 types = all_samples['fine type'].unique()
 
-types_dfs = [pd.DataFrame(y) for x, y in all_samples.groupby('fine type', as_index=False)] # just last one
-cats_dfs = [pd.DataFrame(y) for x, y in all_samples.groupby('category', as_index=False)]
-
+# lists to store the dataframes in
+types_dfs = [pd.DataFrame(y) for x, y in all_samples.groupby('fine type', as_index=False)] # just last type
+cats_dfs = [pd.DataFrame(b) for a, b in all_samples.groupby('category', as_index=False)]
 print('done split to types and categories')
 
-
+# get all relevant types from list of them
 def get_all(list):
     for t in list:
         if t.startswith('dbo:'):
@@ -40,12 +36,16 @@ def get_all(list):
     return 0
 
 
-# all types
+# get list of all types
 types_all = []
 all_samples['type'].apply(get_all)
-types_all_unique = list(set(types_all))
-print('all types', types_all_unique)
+types_all_unique = list(set(types_all)) #make unique
 
+# dictionaries in which to store classifiers, arranges by type/category
+classifiers_all_cat = dict.fromkeys(categories)
+print('classifiers_all_cat', classifiers_all_cat)
+classifiers_all_typ = dict.fromkeys(types_all_unique)
+print('classifiers_all_typ', classifiers_all_typ)
 
 def random_sample_ratioed(datafrm, pos_fraction, ratio_pos, ratio_neg):
     # fraction is a ratio e.g.
@@ -54,7 +54,7 @@ def random_sample_ratioed(datafrm, pos_fraction, ratio_pos, ratio_neg):
     # print('len pos', len(positive))
     # print('len neg', len(negative))
     positive_smples = positive.sample(frac=pos_fraction, random_state=0)
-    neg_samples_wanted = ((len(positive_smples) / ratio_pos) * ratio_neg)  # make sure this is correct! do some math!
+    neg_samples_wanted = ((len(positive_smples) / ratio_pos) * ratio_neg)   # work out number of negative samples we need
     fraction2 = neg_samples_wanted / len(negative)
     # print('pos sampled', len(positive_smples))
     # print('neg wanted', neg_samples_wanted)
@@ -67,15 +67,6 @@ def random_sample_ratioed(datafrm, pos_fraction, ratio_pos, ratio_neg):
     new_df = pd.concat([positive_smples, negative_smples])  # append all data together
     new_df2 = new_df.sample(frac=1)  # shuffle dataframe
     return new_df2
-
-
-def train_classifier(train, label):
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                        hidden_layer_sizes=(5, 2), random_state=1)
-    # print('label', label.shape)   # array y of size (n_samples,)
-    # print('train', train.shape)   # array X of size (n_samples, n_features)
-    clf.fit(list(train), label)   # debug from here
-    return clf
 
 
 # choose the number of negative examples trained for each positive example
@@ -100,39 +91,41 @@ def label_polarity_all_typs(row, label, column):
         return "0"  # negative polarity
 
 
-# dictionaries in which to store classifiers, arranges by type/category
-classifiers_all_cat = dict.fromkeys(categories)
-print('classifiers_all_cat', classifiers_all_cat)
+def train_classifier(train, label):
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(5, 2), random_state=1)
+    clf.fit(list(train), label)
+    return clf
 
+
+''' train and store classifiers '''
+# categories
 for df in cats_dfs:
-    copy_ds = all_samples.copy()
+    copy_df = all_samples.copy()
     cat_label = df["category"].unique()
     cat_label = cat_label[0]
-    copy_ds["y"] = copy_ds.apply(lambda row: label_polarity(row, cat_label, "category"), axis=1)    # label +ve and -ve
-    train_set = random_sample_ratioed(copy_ds, 0.80, 1, 1)  # split differently according to pos/neg balance
+    copy_df["y"] = copy_df.apply(lambda row: label_polarity(row, cat_label, "category"), axis=1)    # label +ve and -ve
+    train_set = random_sample_ratioed(copy_df, 0.80, 1, 1)  # split differently according to pos/neg balance
     X = train_set[vector_component]
     y = train_set["y"]
+    # print(len(X))   # these lengths differ so can't be the same data the classifier is trained on every time
+    # print(len(y))
     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
     classifiers_all_cat[cat_label] = classifier
 
-print(len(types_all_unique)) # 310 unique types
 
-# dictionaries in which to store classifiers, arranges by type/category
-classifiers_all_typ = dict.fromkeys(types_all_unique)
-print('classifiers_all_typ', classifiers_all_typ)
-
-# all types: not just last type
+# all types
 for typ_label in types_all_unique:
-    copy_ds2 = all_samples.copy()
+    copy_df2 = all_samples.copy()
     print('type label', typ_label)
-    copy_ds2["y"] = copy_ds2.apply(lambda row: label_polarity_all_typs(row, typ_label, 'type'), axis=1)  # label -/+
-    train_set = random_sample_ratioed(copy_ds2, 0.80, 1, 1)  # split differently according to pos/neg balance
+    copy_df2["y"] = copy_df2.apply(lambda row: label_polarity_all_typs(row, typ_label, 'type'), axis=1)  # label -/+
+    train_set = random_sample_ratioed(copy_df2, 0.80, 1, 1)  # split differently according to pos/neg balance
     X = train_set[vector_component]
     y = train_set["y"]
     classifier = train_classifier(X, y)  # need to convert vector from list of arrays to matrix
     classifiers_all_typ[typ_label] = classifier
 
-# print('classifiers_all_cat', classifiers_all_cat)
+print('classifiers_all_cat', classifiers_all_cat)
 pickl('classifiers/classifiers_all_cat_ALL', classifiers_all_cat)
 
 print('classifiers_all_typ', classifiers_all_typ)
